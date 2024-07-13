e9f5a3fba7178e795f31abc46f5680aaef45f4c4
Sree Keerthi <137507898+sreekeerthie9@users.noreply.github.com>
2024-07-13 20:32:17 +0530

    Time: 36 ms (80%), Space: 57.8 MB (95%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.java
+++ 2751-robot-collisions/2751-robot-collisions.java
@@ -1,44 +1,50 @@
 class Solution {
-    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+
+    public List<Integer> survivedRobotsHealths(
+        int[] positions,
+        int[] healths,
+        String directions
+    ) {
         int n = positions.length;
-        List<int[]> robots = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            robots.add(new int[]{positions[i], healths[i], directions.charAt(i), i});
-        }
-        Collections.sort(robots,(a,b)->Integer.compare(a[0],b[0]));
-        Stack<int[]> stack = new Stack<>();
-        for(int[] robo : robots){
-            if(robo[2]=='R'|| stack.isEmpty() || stack.peek()[2]=='L'){
-                stack.push(robo);
-                continue;
-            }
-            if(robo[2]=='L'){
-                boolean add = true;
-                while(!stack.isEmpty() && stack.peek()[2]=='R' && add){
-                    int last = stack.peek()[1];
-                    if(robo[1]>last){
-                        stack.pop();
-                        robo[1] -= 1;
-                    }
-                    else if(robo[1]<last){
-                        stack.peek()[1] -= 1;
-                        add= false;
-                    }
-                    else{
-                        stack.pop();
-                        add = false;
-                    }
-                }
-                if(add){
-                    stack.push(robo);
-                }
-            }
-        }
-        List<int[]> resultli = new ArrayList<>(stack);
-        resultli.sort(Comparator.comparingInt(a->a[3]));
+        Integer[] indices = new Integer[n];
         List<Integer> result = new ArrayList<>();
-        for(int[] robo : resultli){
-            result.add(robo[1]);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int index = 0; index < n; ++index) {
+            indices[index] = index;
+        }
+
+        Arrays.sort(
+            indices,
+            (lhs, rhs) -> Integer.compare(positions[lhs], positions[rhs])
+        );
+
+        for (int currentIndex : indices) {
+            if (directions.charAt(currentIndex) == 'R') {
+                stack.push(currentIndex);
+            } else {
+                while (!stack.isEmpty() && healths[currentIndex] > 0) {
+                    int topIndex = stack.pop();
+                    if (healths[topIndex] > healths[currentIndex]) {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        stack.push(topIndex);
+                    } else if (healths[topIndex] < healths[currentIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+
+        
+        for (int index = 0; index < n; ++index) {
+            if (healths[index] > 0) {
+                result.add(healths[index]);
+            }
         }
         return result;
     }
