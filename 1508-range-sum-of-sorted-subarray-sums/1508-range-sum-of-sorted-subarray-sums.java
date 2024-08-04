65c9abbdd2708d9d2c42a8fbd9318a2c049cae9e
Sree Keerthi <137507898+sreekeerthie9@users.noreply.github.com>
2024-08-04 20:18:10 +0530

    Time: 3 ms (94.59%), Space: 41.8 MB (86.04%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,27 +1,40 @@
+import java.util.*;
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            new Comparator<int[]>(){
-                public int compare(int[] o1,int[] o2){
-                    return o1[0] - o2[0];
-                }
-            }
-        );
-        for(int i=0;i<n;i++){
-            pq.add(new int[] {nums[i],i});
-        }
-
-        int sum = 0;
-        for(int i=1;i<=right;i++){
-            int[] p = pq.poll();
-            if(i>=left){
-                sum = (sum+p[0])%1000000007;
-            }
-            if(p[1]<n-1){
-                p[0] += nums[++p[1]];
-                pq.add(p);
-            }
-        }
-        return sum;
+        long res = (bs(nums,n,right) - bs(nums,n,left-1))%1000000007;
+        return (int)(res+1000000007)%1000000007;
     }
-}
+    static long bs(int nums[],int n,int k){
+        int lo = Arrays.stream(nums).min().getAsInt();
+        int hi = Arrays.stream(nums).sum();
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(countAndSum(nums,n,mid).getKey()>=k){
+                hi = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
+        Map.Entry<Integer,Long> result = countAndSum(nums,n,lo);
+        return result.getValue()-lo*(result.getKey()-k);
+
+    }
+    static Map.Entry<Integer,Long> countAndSum(int nums[],int n,int target){
+        int count = 0;
+        long cs = 0;
+        long ws = 0;
+        long ts = 0;
+        for(int j=0,i=0;j<n;j++){
+            cs+=nums[j];
+            ws+=nums[j]*(j-i+1);
+            while(cs>target){
+                ws-=cs;
+                cs-=nums[i++];
+            }
+            count+=j-i+1;
+            ts+=ws;
+        }
+        return new AbstractMap.SimpleEntry<>(count,ts);
+    }
+    }
