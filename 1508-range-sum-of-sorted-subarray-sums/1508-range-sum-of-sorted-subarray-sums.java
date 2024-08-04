9c6d5282be3a0bfea9c203981c6991cf6a552a71
Sree Keerthi <137507898+sreekeerthie9@users.noreply.github.com>
2024-08-04 20:14:23 +0530

    Time: 63 ms (86.04%), Space: 42.1 MB (83.78%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,20 +1,26 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        int l = n*(n+1)/2;
-        int ar[] = new int[l];
-        int k=0;
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                ar[k] = sum;
-                k++;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            new Comparator<int[]>(){
+                public int compare(int[] o1,int[] o2){
+                    return o1[0] - o2[0];
+                }
             }
+        );
+        for(int i=0;i<n;i++){
+            pq.add(new int[] {nums[i],i});
         }
-        Arrays.sort(ar);
+
         int sum = 0;
-        for(int i=left-1;i<=right-1;i++){
-            sum = (sum+ar[i])%1000000007;
+        for(int i=1;i<=right;i++){
+            int[] p = pq.poll();
+            if(i>=left){
+                sum = (sum+p[0])%1000000007;
+            }
+            if(p[1]<n-1){
+                p[0] += nums[++p[1]];
+                pq.add(p);
+            }
         }
         return sum;
     }
